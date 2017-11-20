2d4c24f09aee81a6d4f041a4828f828cc4efe0ca
MeowSterNya <33819172+MeowSterNya@users.noreply.github.com>
2017-11-20 14:51:58 -0400

    index update
    fixed audit and report html to php

---
+++ data/lecturer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.4
+ */
+
+/**
+ * Database `csclubs`
+ */
+
+/* `csclubs`.`lecturer` */
+$lecturer = array(
+  array('LID' => '1','FirstName' => 'Aurel','LastName' => 'Liddell','Gender' => 'Female','Email' => 'aurel.liddell@uog.edu.gy','ContactNumber' => '616-4145','AccessLevel' => '1','Username' => 'Penny','Password' => 'd0af038853bde3b5b99b09dddf424a30'),
+  array('LID' => '2','FirstName' => 'Penelope','LastName' => 'DeFreitas','Gender' => 'Female','Email' => 'penelope.defreitas@uog.edu.gy','ContactNumber' => '644-1627','AccessLevel' => '1','Username' => '','Password' => '')
+);
---
+++ includes/functions_accessLog.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+
+	function insertLogin($userID) {
+		$success = false;
+		//	Get current date-time in MySQL format
+		$nowTimeStamp = date("Y-m-d H:i:s");
+		//	Get user's IP address
+		$userIP = $_SERVER['REMOTE_ADDR'];
+	
+		$insertLogin_SQL = 'INSERT INTO accesslog (
+										SID, 
+										timeLogin, 
+										IPaddress
+									) VALUES (
+										'.$SID.',
+										"'.$nowTimeStamp.'",
+										"'.$userIP.'"
+									)';
+									
+			if (mysql_query($insertLogin_SQL))  {	
+				$success = true;
+			} else {
+				$success = $insertLogin_SQL."<br />".mysql_error();		
+			}	
+			
+		return $success;	
+	}
+
+
+?>
